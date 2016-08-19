5f3fddf411dba6b799862a72347f9ea5d9446afe
Damian Orzepowski <damian.orzepowski@docusafe.pl>
2016-08-20 00:08:58 +0200

    add some libs to bower and use jquery with requirejs

--- app/app.js
+++ app/app.js
@@ -1,3 +1,15 @@
 (function () {
+    require.config({
+        paths:{
+            "jquery":"libs/jquery/dist/jquery.min"
+        },
+        shim: {
 
+        }
+
+    });
+
+    define(['require','view1'],function(require){
+
+    })
 })();
---
+++ app/view1.js
@@ -0,0 +1,5 @@
+(function () {
+    define(['jquery'],function ($) {
+     $('body').append("<h1>VIEW 1</h1>")
+    })
+})();
