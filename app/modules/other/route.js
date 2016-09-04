8512593e8f0bce45381a4e61e7d4816a68455e52
Damian Orzepowski <info@softwarelab.com.pl>
2016-09-04 22:08:25 +0200

    Create simple templateProvider

--- app/modules/other/route.js
+++ app/modules/other/route.js
@@ -1,4 +1,6 @@
-define(["./module"],function (app) {
+define(["./module", "require"], function (app, require) {
+
+    var count = 0;
 
     app.config(['$stateProvider',function ($stateProvider) {
         $stateProvider.state("main.oth",{
@@ -8,7 +10,31 @@ define(["./module"],function (app) {
                     templateUrl: "modules/other/other.html"
                 }
             }
-        });
+        }).state("main.oth.changeable", {
+            url: "/test",
+            views: {
+                'content@': {
+                    templateProvider: function () {
+                        var color = "orange";
+                        var text = "ON";
+                        if (count % 4 == 0) {
+                            var color = "red";
+                            var text = "SHOW";
+                        } else if (count % 4 == 1) {
+                            var color = "green";
+                            var text = "MUST";
+                        } else if (count % 4 == 2) {
+                            var color = "YELLOW";
+                            var text = "GO";
+                        }
+                        count++;
+                        return '<H1 style="color: ' + color + ';">' + text + '</H1><a ui-sref="main.oth.changeable" ui-sref-opts="{reload:true}">GO</a>'
+                    }
+                }
+            }
+        })
+
+        ;
     }]);
 
 });
