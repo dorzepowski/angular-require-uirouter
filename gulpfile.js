cae5f956b16bbbebae4164317c45a898c6978477
Damian Orzepowski <info@softwarelab.com.pl>
2016-09-05 19:05:35 +0200

    Create gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
+var del = require('del');
+var runSequence = require('run-sequence');
 
 gulp.task('serve', function() {
     gulp.src('app')
@@ -9,3 +11,20 @@ gulp.task('serve', function() {
                 open: true
         }));
 });
+
+
+gulp.task('clean', function () {
+    return del(["dist"], {dot: true});
+});
+
+gulp.task('copy:main', function () {
+    gulp.src("app/main/**/*").pipe(gulp.dest("dist"));
+});
+
+gulp.task('copy:modules', function () {
+    gulp.src("app/modules/**/*", {base: "app"}).pipe(gulp.dest("dist"));
+});
+
+gulp.task('build', ['clean', 'copy:main', 'copy:modules'], function (cb) {
+});
+
